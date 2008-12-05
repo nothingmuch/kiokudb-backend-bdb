b8b0ad56cf5f715a7a5008c83acc80e01c420335
Yuval Kogman <nothingmuch@woobling.org>
2008-12-05 14:24:21 +0200

    version bump

--- lib/KiokuDB/Backend/BDB.pm
+++ lib/KiokuDB/Backend/BDB.pm
@@ -20,7 +20,7 @@ use namespace::clean -except => 'meta';
 # this should be generic (work with both c_get and c_pget, and the various
 # flags)
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 with qw(
     KiokuDB::Backend
